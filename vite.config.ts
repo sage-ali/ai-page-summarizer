9fd53883a861c6eba023eaa4e538062ed39ad90b
sage-ali <81039794+sage-ali@users.noreply.github.com>
2026-05-03 01:21:49 +0100

    chore: project tooling, and tsconfigs

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
